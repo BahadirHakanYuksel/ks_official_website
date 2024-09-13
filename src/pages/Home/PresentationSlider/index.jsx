357d4ceb4fc01980e99d39ef4e58bfa8932aa501
Bahadır Hakan <bhyuksel0@gmail.com>
2024-09-13 17:22:16 +0300

    added telNo on slider

--- src/pages/Home/PresentationSlider/index.jsx
+++ src/pages/Home/PresentationSlider/index.jsx
@@ -40,16 +40,19 @@ function PresentationSlider() {
       id: 0,
       url: i18n.language === "en" ? "ks_1.png" : "sh_1.png",
       link: "page 1",
+      hrefUrl: "tel:05511234567",
     },
     {
       id: 1,
       url: i18n.language === "en" ? "ks_2.png" : "ks_2tr.png",
       link: "page 2",
+      hrefUrl: false,
     },
     {
       id: 2,
       url: i18n.language === "en" ? "ks_3.png" : "ks_3tr.png",
       link: "page 3",
+      hrefUrl: false,
     },
   ];
 
@@ -57,7 +60,8 @@ function PresentationSlider() {
     <div className={classNames("rounded-md h-auto presenteSlider relative")}>
       <div className="flex gap-2.5 relative sliderBox overflow-hidden rounded-lg w-full aspect-video">
         {sliderData.map((sliderSection) => (
-          <motion.div
+          <motion.a
+            href={sliderSection.hrefUrl}
             key={sliderSection.id}
             className={classNames(
               "aspect-video absolute w-full left-0 top-0 opacity-0 invisible text-white bg-gray-700 transition-all",
@@ -80,7 +84,7 @@ function PresentationSlider() {
                 />
               )}
             </AnimatePresence>
-          </motion.div>
+          </motion.a>
         ))}
       </div>
       <button
